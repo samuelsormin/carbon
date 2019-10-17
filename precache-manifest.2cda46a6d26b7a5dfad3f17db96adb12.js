da111cd271e269ff2b4ee3b7a633ef036ca6dd4f
Samuel Ebenezer <christlegacies@gmail.com>
2019-10-17 19:20:13 +0700

    Updates

---
+++ precache-manifest.2cda46a6d26b7a5dfad3f17db96adb12.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28806216dd606227b6752814deabbd4a",
+    "url": "/carbon/index.html"
+  },
+  {
+    "revision": "921cb18c56b8ffd5b640",
+    "url": "/carbon/static/css/main.b4001af9.chunk.css"
+  },
+  {
+    "revision": "1d08693a54fb1c2d60e6",
+    "url": "/carbon/static/js/2.a3511e15.chunk.js"
+  },
+  {
+    "revision": "921cb18c56b8ffd5b640",
+    "url": "/carbon/static/js/main.2041531f.chunk.js"
+  },
+  {
+    "revision": "ff6d7805d186766023e7",
+    "url": "/carbon/static/js/runtime~main.0d799092.js"
+  },
+  {
+    "revision": "167c61cc32121db6b28e8dc5445afd6e",
+    "url": "/carbon/static/media/back-to-school-slide.167c61cc.png"
+  },
+  {
+    "revision": "8c9caf9c49c842af622798ecb4d12a95",
+    "url": "/carbon/static/media/buku1.8c9caf9c.jpg"
+  },
+  {
+    "revision": "0056e3ab2379c4829cf72cac393b31d7",
+    "url": "/carbon/static/media/buku2.0056e3ab.jpg"
+  },
+  {
+    "revision": "d4fd7b94155e710de12626b5f9418b8c",
+    "url": "/carbon/static/media/buku3.d4fd7b94.jpg"
+  },
+  {
+    "revision": "ae3b4ee221e5c3519f00d3fa6e4353db",
+    "url": "/carbon/static/media/buku4.ae3b4ee2.jpg"
+  },
+  {
+    "revision": "2061076b699cd334c8f0a067ecee5d87",
+    "url": "/carbon/static/media/bukukecil1.2061076b.jpg"
+  },
+  {
+    "revision": "ce64dad568c4c3c39e32e822ead6f4a7",
+    "url": "/carbon/static/media/bukukecil2.ce64dad5.jpg"
+  },
+  {
+    "revision": "af2b6cc4d1ea11fdaa87a7ff6b2c98ad",
+    "url": "/carbon/static/media/bukukecil3.af2b6cc4.jpg"
+  },
+  {
+    "revision": "c084c11c87d54576e713b103ba3e053c",
+    "url": "/carbon/static/media/bukukecil4.c084c11c.jpg"
+  },
+  {
+    "revision": "c2c1a1b62928cad10c43f42c666f2870",
+    "url": "/carbon/static/media/bukukecil5.c2c1a1b6.jpg"
+  },
+  {
+    "revision": "6feb53973f56a8fd24a636b49c233e71",
+    "url": "/carbon/static/media/bukukecil6.6feb5397.jpg"
+  },
+  {
+    "revision": "ea65037bae937de0b7cd12d92cff9deb",
+    "url": "/carbon/static/media/bukukecil7.ea65037b.jpg"
+  },
+  {
+    "revision": "1558ae408553d7dfb3700486fb687d73",
+    "url": "/carbon/static/media/bukukecil8.1558ae40.jpg"
+  },
+  {
+    "revision": "b161a2081b99f138f9dcf108f9174245",
+    "url": "/carbon/static/media/new-books.b161a208.png"
+  },
+  {
+    "revision": "3e5a2803ec5d03e9a6ab0bcba955ef8b",
+    "url": "/carbon/static/media/popular-books.3e5a2803.png"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/carbon/static/media/themify.2c454669.eot"
+  },
+  {
+    "revision": "9c8e96ecc7fa01e6ebcd196495ed2db5",
+    "url": "/carbon/static/media/themify.9c8e96ec.svg"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/carbon/static/media/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/carbon/static/media/themify.e23a7dca.ttf"
+  }
+]);
